737f96816e84d3fe7a30cccdd48041a6ac22b60a
alejandra <abarreto@360pmi.com>
2025-10-26 18:06:52 -0500

    add(utils): categories

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -3,6 +3,7 @@ import { MaterialIcons } from "@expo/vector-icons"
 
 import { styles } from "./styles"
 import { colors } from "@/styles/colors"
+import { categories } from "@/utils/categories"
 
 import { Category } from "@/components/category"
 
@@ -16,7 +17,13 @@ export default function Index() {
         </TouchableOpacity>
       </View>
 
-      <Category />
+      <Category 
+        name={categories[0].name}
+        icon={categories[0].icon} 
+        isSelected 
+      />
+      <Category name="Site" icon="language" isSelected={false} />
+      <Category name="Video" icon="movie" isSelected={false} />
    </View>
   )
 }
