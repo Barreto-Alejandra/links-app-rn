b2a52b1db66fee29206675c281261e845e100fff
alejandra <abarreto@360pmi.com>
2025-10-27 18:42:59 -0500

    add(links): component

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,10 +1,11 @@
-import { View, Image, TouchableOpacity } from "react-native"
+import { View, Image, TouchableOpacity, FlatList, Modal, Text } from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
 
 import { styles } from "./styles"
 import { colors } from "@/styles/colors"
 
 import { Categories } from "@/components/categories"
+import { Link } from "@/components/link"
 
 export default function Index() {
   return (
@@ -16,6 +17,41 @@ export default function Index() {
         </TouchableOpacity>
       </View>
     <Categories />
+    
+    <FlatList
+      data={["1","2", "3", "4"]}
+      keyExtractor={(item) => item}
+      renderItem={() => (
+        <Link 
+          name="Link Project" 
+          url="/" 
+          onDetails={() => console.log("Click")}
+        />
+      )}
+      style={styles.links}
+      contentContainerStyle={styles.linksContent}
+      showsVerticalScrollIndicator={false}
+    />
+    <Modal transparent >
+      <View style={styles.modal}>
+        <View style={styles.modalContent}>
+          <View style={styles.modalHeader}>
+            <Text style={styles.modalCategory}>
+              Course
+            </Text>
+            <TouchableOpacity>
+              <MaterialIcons name="close" size={20} color={colors.gray[400]} />
+            </TouchableOpacity>
+          </View>
+          <Text style={styles.modalLinkName}>
+            Link Project
+          </Text>
+          <Text style={styles.modalUrl}>
+              https://
+          </Text>
+        </View>
+      </View>
+    </Modal>
    </View>
   )
 }
