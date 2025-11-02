93834ea18ed7a67c48a73118fe8852dd97353008
alejandra <abarreto@360pmi.com>
2025-11-02 14:06:47 -0500

    update(add): component

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -1,19 +1,23 @@
 import { View, Text, TouchableOpacity } from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
+import { router } from "expo-router"
 
 import { styles } from "./styles"
 import { colors } from "@/styles/colors"
+
+import { Categories } from "@/components/categories"
 
 export default function Add() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => router.back()}>
           <MaterialIcons name="arrow-back" size={32} color={colors.gray[200]} />
         </TouchableOpacity>
         <Text style={styles.title}>NEW</Text>
       </View>
       <Text style={styles.label}>Select category</Text>
+      <Categories />
     </View> 
   )
 }
--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,5 +1,6 @@
 import { View, Image, TouchableOpacity, FlatList, Modal, Text } from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
+import { router } from "expo-router"
 
 import { styles } from "./styles"
 import { colors } from "@/styles/colors"
@@ -13,7 +14,7 @@ export default function Index() {
    <View style={styles.container}>
       <View style={styles.header}>
         <Image source={require("@/assets/logo.png")} style={styles.logo} />
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => router.navigate("/add")}>
           <MaterialIcons name="add" size={32} color={colors.green[300]} />
         </TouchableOpacity>
       </View>
@@ -25,7 +26,7 @@ export default function Index() {
       renderItem={() => (
         <Link 
           name="Link Project" 
-          url="/" 
+          url="https://reactnative.dev/docs/environment-setup" 
           onDetails={() => console.log("Click")}
         />
       )}
@@ -33,7 +34,7 @@ export default function Index() {
       contentContainerStyle={styles.linksContent}
       showsVerticalScrollIndicator={false}
     />
-    <Modal transparent >
+    <Modal transparent visible={false} >
       <View style={styles.modal}>
         <View style={styles.modalContent}>
           <View style={styles.modalHeader}>
